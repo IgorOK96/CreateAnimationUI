a6b87808e44a9e43c40978552fc1b92228328a02
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-19 15:29:43 +0200

    Initial Commit

---
+++ CreateAnimationUI/CreateAnimationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAnimationUIApp.swift
+//  CreateAnimationUI
+//
+//  Created by user246073 on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CreateAnimationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
